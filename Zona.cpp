73321855d8a5c0f084006b18428e3b075f8d7dd8
sudo-john-blossom <73914719+sudo-joao-carvalho@users.noreply.github.com>
2023-11-08 10:34:23 +0000

    Update Zona.cpp

--- Zona.cpp
+++ Zona.cpp
@@ -41,66 +41,58 @@ Aparelho* Zona::adicionaAparelho(const char& tipoDerivado) {
 
     if(tipoDerivado == 'a'){ //aquecedor
         aparelhos.push_back(new Aquecedor());
-        return aparelhos.back();
     }
 
     if(tipoDerivado == 's'){ //aspersor
         aparelhos.push_back(new Aspersor());
-        return aparelhos.back();
     }
 
     if(tipoDerivado == 'r'){ //refrigerador
         aparelhos.push_back(new Refrigerador());
-        return aparelhos.back();
     }
-
 
     if(tipoDerivado == 'l'){ //lampada
         aparelhos.push_back(new Lampada());
-        return aparelhos.back();
+    }else{
+        return nullptr;
     }
 
-    return nullptr;
+    return aparelhos.back();
 }
 
 Sensor* Zona::adicionaSensor(const char& tipoDerivado) {
 
     if(tipoDerivado == 't'){ //temperatura
         sensores.push_back(new SensorTemperatura());
-        return sensores.back();
     }
 
     if(tipoDerivado == 'v'){ //movimento
         sensores.push_back(new SensorMovimento());
-        return sensores.back();
     }
 
     if(tipoDerivado == 'm'){ //luminosidade
         sensores.push_back(new SensorLuminosidade());
-        return sensores.back();
     }
 
     if(tipoDerivado == 'd'){ //radiacao
         sensores.push_back(new SensorRadiacao());
-        return sensores.back();
     }
 
     if(tipoDerivado == 'h'){ //humidade
         sensores.push_back(new SensorHumidade());
-        return sensores.back();
     }
 
     if(tipoDerivado == 'o'){ //som
         sensores.push_back(new SensorSom());
-        return sensores.back();
     }
 
     if(tipoDerivado == 'f'){ //fumo
         sensores.push_back(new SensorFumo());
-        return sensores.back();
+    }else{
+        return nullptr;
     }
 
-    return nullptr;
+    return sensores.back();
 }
 
 bool Zona::removeEquipamento(const char &tipoEquipamento, const int &idEquipamento) {

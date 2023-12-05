33afce8c3586e84e5f00a86a49d652c977df48b2
sudo-john-blossom <73914719+sudo-joao-carvalho@users.noreply.github.com>
2023-12-05 10:31:18 +0000

    Update Regra.cpp

--- Regras/Regra.cpp
+++ Regras/Regra.cpp
@@ -2,6 +2,9 @@
 // Created by João Carvalho on 12/11/2023.
 //
 
+//sensor ativa processador e o sensor sabe qual e o processador de regras sabe qual e o sensor
+//fazer classes para cada propriedade
+
 #include "Regra.h"
 
 Regra::Regra(Sensor& sensor):sensor(sensor) {}
